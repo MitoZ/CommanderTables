dbf57ea855f72d49808cb0c7609d5bc1137db561
MitoZ <mitozwork@gmail.com>
2016-12-29 19:23:14 +0300

    some changes

--- app/Modules/Page_Modules/Page_UsersList/UsersList/usersList_controller.js
+++ app/Modules/Page_Modules/Page_UsersList/UsersList/usersList_controller.js
@@ -7,10 +7,10 @@ import './usersList.less';
 module.exports = function (angular, usersList) {
   require('./usersList_directives')(angular);
   require('./usersList_services')(angular);
-  function userListController(usersListModels) {
+  const userListController = function (usersListModels) {
     let vm = this;
     vm.users = usersListModels.users;
-  }
+  };
   angular.module('commanderTablesApp')
     .controller('userListController', ['usersListModels', userListController]);
 };
