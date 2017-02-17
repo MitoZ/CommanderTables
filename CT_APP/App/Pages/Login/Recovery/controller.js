ba69312d4ce5194fa334444cb2491fea03879e6e
MitoZ <mitozwork@gmail.com>
2017-02-17 15:19:27 +0300

    add recovery pass page

--- CT_APP/App/Pages/Login/Recovery/controller.js
+++ CT_APP/App/Pages/Login/Recovery/controller.js
@@ -2,13 +2,70 @@
 /**
  * Created by dzmitry.barkouski on 10.01.2017.
  */
+
+import angular from 'angular';
+let dependencies = [
+  'authService',
+  '$state',
+  // '$location',
+  '$mdDialog',
+  'cudFsLoaderService',
+  '$scope'
+];
 export default class LoginRecoverController{
-  constructor() {
+  constructor(authService, $state, $mdDialog, cudFsLoaderService, $scope) {
     let vm = this;
     vm.recModel = {};
+    vm.authService = authService;
+    vm.$state = $state;
+    vm.$mdDialog = $mdDialog;
+    vm.cudFsLoaderService = cudFsLoaderService;
+    vm._$scope = $scope;
   }
   
   recoveryAccount(mail) {
-    
+    let form = this._$scope.recoveryForm,
+      errors = angular.extend({}, form.recoveryAccMail.$error);
+    if (Object.keys(errors).length || !(mail && mail.length)) {
+      this.throwError('Validation failed.');
+    } else {
+      this.cudFsLoaderService.show();
+      this.authService.resetPass(mail).then(
+        (data)=>{
+          console.info('Recovery pass - ', data);
+          this.cudFsLoaderService.hide();
+          this
+            .$mdDialog
+            .show(
+              this.$mdDialog.alert({
+                title: 'Recovery is done.',
+                textContent: 'The Email with instruction was send.',
+                ok: 'Close'
+              })
+            )
+            .finally(()=>{
+              this.$state.go('login.page');
+            });
+        },
+        (error)=>{
+          this.cudFsLoaderService.hide();
+          this.throwError((error && error.message) ? error.message : 'Unknown recovery error.');
+        });
+    }
+  }
+  
+  throwError(error) {
+    this
+      .$mdDialog
+      .show(
+        this.$mdDialog.alert({
+          title: 'Recovery failed',
+          textContent: error,
+          ok: 'Close',
+          theme: 'errors'
+        })
+      );
+    console.warn('some error - ', error); //TODO: Delete this before checkIN
   }
 }
+LoginRecoverController.$inject = dependencies;
